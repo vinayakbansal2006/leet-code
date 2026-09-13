978711eb16e5448d56250fb041a6735cd9f1605c
Vinayak Bansal <bansalvinayak16@gmail.com>
2026-09-13 20:35:26 +0530

    Time: 932 ms (5.07%), Space: 23.9 MB (19.45%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zeros,pos=0;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-               int temp=nums[i];
-                nums[i]=nums[pos];
-                nums[pos]=temp;
-                pos++;
+        for(int j=0;j<n;j++){
+            for(int i=0;i<n-1;i++){
+                if(nums[i]==0){
+                    int temp=nums[i];
+                    nums[i]=nums[i+1];
+                    nums[i+1]=temp;
+                }
+
             }
-            
         }
         
     }
